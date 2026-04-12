7f66bf0cca21526a2d0f1386d9a456ab8e3be640
Claower <140240956+Claower3D@users.noreply.github.com>
2026-04-12 11:05:24 +0600

    fix: use VITE_API_URL in production

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -5,9 +5,8 @@
  */
 
 export const API_URL: string =
-  import.meta.env.PROD || !import.meta.env.VITE_API_URL
-    ? '/api'
-    : import.meta.env.VITE_API_URL;
+  import.meta.env.VITE_API_URL || '/api';
 
 export const USE_MOCK_API: boolean =
   import.meta.env.VITE_USE_MOCK_API === 'true';
+
